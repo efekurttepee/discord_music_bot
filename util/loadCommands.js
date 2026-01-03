adb816efd8019bb12be666e2df9044e98e296b97
Developer <dev@example.com>
2026-01-03 17:49:59 +0300

    Fix command loading bug - use continue instead of return

--- util/loadCommands.js
+++ util/loadCommands.js
@@ -5,7 +5,7 @@ const LoadCommands = () => {
 	return new Promise(async (resolve) => {
 		let slash = await LoadDirectory("slash");
 		let context = await LoadDirectory("context");
-		
+
 		resolve({ slash, context });
 	});
 };
@@ -14,20 +14,21 @@ const LoadDirectory = (dir) => {
 	return new Promise((resolve) => {
 		let commands = [];
 		let CommandsDir = join(__dirname, "..", "commands", dir);
-		
+
 		fs.readdir(CommandsDir, (err, files) => {
 			if (err) {
 				throw err;
 			}
-			
+
 			for (const file of files) {
 				let cmd = require(CommandsDir + "/" + file);
 				if (!cmd || (dir == "context" && !cmd.command)) {
-					return console.log(
+					console.log(
 						"Unable to load Command: " +
 						file.split(".")[0] +
 						", File doesn't have either command",
 					);
+					continue;
 				}
 				if (dir == "context") {
 					commands.push(cmd.command);
