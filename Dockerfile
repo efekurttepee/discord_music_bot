877233f3e83cc20e1f467db5281f6437197435d1
Developer <dev@example.com>
2026-01-03 17:59:24 +0300

    Auto-deploy slash commands on bot startup instead of build time

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM node:20-alpine
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
-RUN npm run deploy
 CMD [ "node", "index.js" ]
--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,34 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const LoadCommands = require("../util/loadCommands");
+
 /**
  *
  * @param {import("../lib/DiscordMusicBot")} client
  */
-module.exports = (client) => {
+module.exports = async (client) => {
 	client.manager.init(client.user.id);
 	client.user.setPresence(client.config.presence);
 	client.log("Successfully Logged in as " + client.user.tag);
+
+	// Auto-deploy slash commands on startup
+	try {
+		client.log("Deploying slash commands...");
+		const rest = new REST({ version: "9" }).setToken(client.config.token);
+		const commands = await LoadCommands().then((cmds) => {
+			return [].concat(cmds.slash).concat(cmds.context);
+		});
+
+		const jsonCommands = commands.map(cmd => cmd.toJSON ? cmd.toJSON() : cmd);
+		client.log(`Deploying ${jsonCommands.length} commands to Discord...`);
+
+		await rest.put(Routes.applicationCommands(client.config.clientId), {
+			body: jsonCommands,
+		});
+
+		client.log(`Successfully deployed ${jsonCommands.length} slash commands!`);
+	} catch (error) {
+		client.error("Failed to deploy slash commands:");
+		client.error(error);
+	}
 };
